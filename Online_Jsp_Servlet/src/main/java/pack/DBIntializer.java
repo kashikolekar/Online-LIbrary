8487a78fec52bc80ba89f2a7d22a441c1aa2ab8d
Kashinath Kolekar <125242548+kashikolekar@users.noreply.github.com>
2023-09-30 09:03:48 +0530

    Online-LIbrary
    The E-Library project is a web-based application developed using Java Servlets, HTML, and CSS. It
    provides users with an online platform to access and manage a digital library of books, journals, and
    other educational resources. The project involves designing an interactive user interface using HTML
    and CSS and implementing server-side functionalities using Java Servlets for user authentication,
    database connectivity, and book management. The application aims to provide users with a convenient
    and user friendly digital library experience, making it easy for them to access and manage educational
    materials online.

---
+++ Online_Jsp_Servlet/src/main/java/pack/DBIntializer.java
@@ -0,0 +1,8 @@
+package pack;
+
+public interface DBIntializer {
+String DRIVER="com.mysql.jdbc.Driver";
+String CON_STRING="jdbc:mysql://localhost:3306/onlinel";
+String USERNAME="root";
+String PASSWORD="Ihsak@123";
+}
